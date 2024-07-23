4caaca2cdc76f228b68090fbd1347c2a49ff4fd2
Maryam Fadaee <maryamfadaee@gmail.com>
2024-07-23 16:19:06 -0400

    remove port number

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "https://wealthpath-385e08c18cf4.herokuapp.com:5000"
+      url: "https://wealthpath-385e08c18cf4.herokuapp.com"
     },
     {
       url: "http://localhost:5000" 

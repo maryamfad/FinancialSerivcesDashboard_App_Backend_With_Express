2460eb74b94d66933a06538aedc7c3c69e86445a
Maryam Fadaee <maryamfadaee@gmail.com>
2024-08-02 15:48:46 -0400

    add trade type and correct order type

--- models/Order.js
+++ models/Order.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   stockSymbol: { type: String, required: true },
-  orderType: { type: String, enum: ['buy', 'sell'], required: true },
+  tradeType: { type: String, enum: ['buy', 'sell'], required: true },
+  orderType: { type: String, enum: ['market', 'limit', 'stop'], required: true },
   quantity: { type: Number, required: true },
   price: { type: Number, required: true },
   status: { type: String, enum: ['pending', 'executed', 'canceled'], default: 'pending' },

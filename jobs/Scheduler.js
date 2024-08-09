118bf3c5c06ef0ceb7f6106c54841b41d0bac399
Maryam Fadaee <maryamfadaee@gmail.com>
2024-08-09 00:11:16 -0400

    check if db is connected

--- jobs/Scheduler.js
+++ jobs/Scheduler.js
@@ -1,10 +1,23 @@
 import cron from "node-cron";
 import Portfolio from "../models/Portfolio.js";
 import updatePortfolioPerformance from "../utils/updatePortfolioPerformance.js";
-
+function checkDatabaseConnection() {
+	if (mongoose.connection.readyState === 1) {
+		// 1 means connected
+		return Promise.resolve();
+	} else {
+		return mongoose
+			.connect(process.env.MONGO_DB_URI + uri_tail)
+			.then(() => console.log("MongoDB connected"))
+			.catch((err) =>
+				console.error("Error with MongoDB Connection", err)
+			);
+	}
+}
 cron.schedule("*/5 * * * *", async () => {
 	console.log("Cron job executed every 5 minutes");
 	try {
+		await checkDatabaseConnection();
 		const portfolios = await Portfolio.find();
 
 		for (const portfolio of portfolios) {

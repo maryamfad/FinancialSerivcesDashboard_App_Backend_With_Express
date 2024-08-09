baeca62402384b64cf4836470d6fb995b54c72f1
Maryam Fadaee <maryamfadaee@gmail.com>
2024-08-08 23:59:57 -0400

    reduce time in scheduler

--- jobs/Scheduler.js
+++ jobs/Scheduler.js
@@ -2,8 +2,8 @@ import cron from "node-cron";
 import Portfolio from "../models/Portfolio.js";
 import updatePortfolioPerformance from "../utils/updatePortfolioPerformance.js";
 
-cron.schedule("*/30 * * * *", async () => {
-	console.log("Cron job executed every 30 minutes");
+cron.schedule("*/5 * * * *", async () => {
+	console.log("Cron job executed every 5 minutes");
 	try {
 		const portfolios = await Portfolio.find();
 

038c43c55b479c5ef37e8be42793aadcb95974ac
Maryam Fadaee <maryamfadaee@gmail.com>
2024-08-12 22:32:57 -0400

    change the env name

--- utils/fetchCurrentMarketValue.js
+++ utils/fetchCurrentMarketValue.js
@@ -4,9 +4,9 @@ const fetchCurrentMarketValue = async (symbol) => {
 	}
 	console.log(
 		"FINANCIAL_MODELING_PREP_API_KEY:",
-		process.env.FINANCIAL_MODELING_PREP //for testing purposes
+		process.env.FINANCIAL_MODELING_PREP_API_KEY //for testing purposes
 	);
-	const url = `https://financialmodelingprep.com/api/v3/quote-short/${symbol}?apikey=${process.env.FINANCIAL_MODELING_PREP}`;
+	const url = `https://financialmodelingprep.com/api/v3/quote-short/${symbol}?apikey=${process.env.FINANCIAL_MODELING_PREP_API_KEY}`;
 	try {
 		const response = await fetch(url, {
 			method: "GET",
